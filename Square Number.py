45c548b93e688c9794343bc34a254df3da5e4207
ebnezerdaniel <68187483+ebnezerdaniel@users.noreply.github.com>
2020-07-12 15:57:46 +0200

    Square Root and Number
    July 12th

---
+++ Square Number.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Square of a number
+
+
+# In[2]:
+
+
+for i in range (1,20,2):
+    print('Squareroot of',i,'=',i**2)
+    if i==25:
+        break
+
---
+++ Square Root.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Python Program to Find the Square Root
+
+
+# In[2]:
+
+
+import math
+t=int(input('Enter the number:'))
+z=t**0.5
+print('Squareroot of the number:',z)
+
