be36910edec46dcd946d28e65d54315cf51bd484
Daniel Raj Kummari <daniel-raj.kummari@stud.uni-bamberg.de>
2020-07-12 16:53:05 +0200

    'Even'

---
+++ EvenorOdd.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Check if a Number is Odd or Even
+
+
+# In[2]:
+
+
+Number=int(input('Enter the Number:'))
+
+
+# In[3]:
+
+
+if Number%2==0:
+    print('even')
+else:
+    print('odd')
+
