37b7468af798394a7030bba37af4fc6dbb0c1388
ebnezerdaniel <68187483+ebnezerdaniel@users.noreply.github.com>
2020-07-12 15:56:39 +0200

    Add two Numbers
    July 12th

---
+++ Add Two Numbers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Python Program to Add Two Numbers
+
+
+# In[ ]:
+
+
+q=int(input('Enter the first number:'))
+w=int(input('Enter the second number:'))
+e=q+w
+print('Addition of two numbers:',e)
+
