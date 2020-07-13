7741a5f744b7cdfd72b5b710b16353edbf0c7a2f
Daniel Raj Kummari <daniel-raj.kummari@stud.uni-bamberg.de>
2020-07-14 00:21:32 +0200

    'Tab'

---
+++ TabandNewline.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+#Printing a poem using \n(new line) and \t for the tab spacing
+
+
+# In[2]:
+
+
+print ('"Twinkle, twinkle, little star,\n\t How I wonder what you are! \n\t\t Up above the world so high,\n\t\t\t Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\tHow I wonder what you are"')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
